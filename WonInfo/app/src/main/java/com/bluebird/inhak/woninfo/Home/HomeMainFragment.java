0ab641cb8c8bddb5d8b88219fee9a8d8bf409192
DESKTOP-APN2QJI\wku <ccc901459@gmail.com>
2018-11-19 22:30:36 +0900

    no message

--- WonInfo/app/src/main/java/com/bluebird/inhak/woninfo/Home/HomeMainFragment.java
+++ WonInfo/app/src/main/java/com/bluebird/inhak/woninfo/Home/HomeMainFragment.java
@@ -21,6 +21,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 
 import com.bluebird.inhak.woninfo.Community.Board1.BoardListAdapter;
@@ -241,7 +242,7 @@ public class HomeMainFragment extends Fragment {
 
 
 
-        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.Listview);
+        ListView recyclerView = (ListView) view.findViewById(R.id.Listview);
         //각 Item들이 RecyclerView 의 전체 크기를 변경하지 않는다면
         //setHasFixedSize() 함수를 사용해서 성능을 개선할 수 있습니다.
         //변경될 가능성이 있다면 false로, 없다면 true를 설정해주세요
