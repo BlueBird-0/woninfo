2d89a255c637d7746167ea64ce022c3c5fade75e
DESKTOP-APN2QJI\wku <ccc901459@gmail.com>
2018-11-20 22:28:09 +0900

    Merge remote-tracking branch 'origin/bluebird-0' into sori
    # Conflicts:
    #	WonInfo/.idea/caches/build_file_checksums.ser
    #	WonInfo/app/src/main/res/layout/community_item_comment.xml

---
+++ WonInfo/app/src/main/java/com/bluebird/inhak/woninfo/Popupgirloffically.java
@@ -0,0 +1,29 @@
+package com.bluebird.inhak.woninfo;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Popupgirloffically extends AppCompatActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.popup_girl);
+        Intent intent = new Intent(this.getIntent());
+
+        Button button = (Button) findViewById(R.id.popup_girl_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://intra.wku.ac.kr"));
+                startActivity(intent);
+            }
+        });
+
+    }
+}
+
+
