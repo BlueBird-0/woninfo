44438ac171b4d5cb4c22399e8e866f02ac79a5b6
DESKTOP-QO528E4\hyeongJincom <gudwls0207@gmail.com>
2018-11-21 01:56:43 +0900

    #11.21 게시판 글 작성 때 들어가는 값이 없으면 막는 걸 만들었어요 이거 날아가면 엉엉 울거야

--- WonInfo/app/src/main/java/com/bluebird/inhak/woninfo/Home/HomeMainFragment.java
+++ WonInfo/app/src/main/java/com/bluebird/inhak/woninfo/Home/HomeMainFragment.java
@@ -78,10 +78,10 @@ public class HomeMainFragment extends Fragment {
 
 
         ArrayList<String> data = new ArrayList<>(); //이미지 url를 저장하는 arraylist
-        data.add("https://upload.wikimedia.org/wikipedia/en/thumb/2/24/SpongeBob_SquarePants_logo.svg/1200px-SpongeBob_SquarePants_logo.svg.png");
-        data.add("http://nick.mtvnimages.com/nick/promos-thumbs/videos/spongebob-squarepants/rainbow-meme-video/spongebob-rainbow-meme-video-16x9.jpg?quality=0.60");
-        data.add("http://nick.mtvnimages.com/nick/video/images/nick/sb-053-16x9.jpg?maxdimension=&quality=0.60");
-        data.add("https://www.gannett-cdn.com/-mm-/60f7e37cc9fdd931c890c156949aafce3b65fd8c/c=243-0-1437-898&r=x408&c=540x405/local/-/media/2017/03/14/USATODAY/USATODAY/636250854246773757-XXX-IMG-WTW-SPONGEBOB01-0105-1-1-NC9J38E8.JPG");
+        data.add("http://www.wku.ac.kr/wp-content/themes/wku/common/img/main/www-20180727-ipsi-2.jpg");
+        data.add("http://www.wku.ac.kr/wp-content/themes/wku/common/img/main/www-20181119-prime.jpg");
+        data.add("http://www.wku.ac.kr/wp-content/themes/wku/common/img/main/www-20181022-autumn.jpg");
+        data.add("http://www.wku.ac.kr/wp-content/themes/wku/common/img/main/www-20181029-aut_botonic_lib.jpg");
 
         autoViewPager = (AutoScrollViewPager) view.findViewById(R.id.autoViewPager);
         AutoScrollAdapter scrollAdapter = new AutoScrollAdapter(getContext(), data);
